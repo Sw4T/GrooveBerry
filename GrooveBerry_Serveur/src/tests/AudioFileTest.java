a2e1c8e8362eeeef07f12fd93e737c6ff777850b
gmp0836a <gmp0836a@r-info-rubi-l19.iut.iut-tlse3.fr>
2015-01-15 18:20:12 +0100

    Added tests, gitignore

--- GrooveBerry_Serveur/src/tests/AudioFileTest.java
+++ GrooveBerry_Serveur/src/tests/AudioFileTest.java
@@ -6,19 +6,36 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import files.AudioFile;
+
 public class AudioFileTest {
 
+	private AudioFile audioFile;
+	
 	@Before
 	public void setUp() throws Exception {
+		audioFile = new AudioFile("audio/Bob Marley - Jammin.mp3");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		audioFile = null;
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_mute() {
+		audioFile.mute();
+		assertEquals(true, audioFile.isMuted());
+		audioFile.mute();
+		assertEquals(false, audioFile.isMuted());
+	}
+	
+	@Test
+	public void test_play() {
+		audioFile.play();
+		assertEquals(true, audioFile.isPlaying());
+		audioFile.stop();
+		assertEquals(false, audioFile.isPlaying());
 	}
 
 }
