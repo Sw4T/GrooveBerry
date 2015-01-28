0c13d5a8c3d39f520fc86b9a82d423457bee4077
gmp0836a <gmp0836a@r-info-rubi-l19.iut.iut-tlse3.fr>
2015-01-28 14:53:27 +0100

    I love LeBlanc (gogo test plz)

--- GrooveBerry_Serveur/src/network/Server.java
+++ GrooveBerry_Serveur/src/network/Server.java
@@ -13,26 +13,48 @@ public class Server {
 	public static final int SERVER_PORT = 12345;
 	private ServerSocket serveur;
 	private Socket socketCliente;
+	private BufferedReader bufferRead;
 	
 	public Server() throws IOException {
 		serveur = new ServerSocket(SERVER_PORT);
 	}
 	
+	//Attente d'une connexion cliente et création du buffer de lecture
 	public void waitConnection() throws IOException {
 		socketCliente = serveur.accept();
 		System.out.println("Client " + socketCliente.getInetAddress() + " has connected !");
+		bufferRead = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
 	}
 	
+	//Utilisation d'un nouveau thread pour permettre d'effectuer les tests en parallèle
+	public void waitConnectionForTest() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					socketCliente = serveur.accept();
+					System.out.println("Client " + socketCliente.getInetAddress() + " has connected !");
+					BufferedReader buffer = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+					setBufferReader(buffer);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+	
+	//Reçoit des chaines de caractères venant du client, tant que "exit" n'a pas été reçu
 	public void getTreatmentFromRemote() throws IOException, InterruptedException 
 	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
-		String audioFileName = br.readLine(); //Lecture du fichier audio allant être utilisé
+		if (bufferRead == null)
+			return;
+		String audioFileName = bufferRead.readLine(); //Lecture du fichier audio allant être utilisé
 		String constant;
 		System.out.println("File used : " + audioFileName);
 		
 		AudioFile audioFileToPlay = new AudioFile(audioFileName);
 		do {
-			constant = br.readLine();
+			constant = bufferRead.readLine();
 			execute(audioFileToPlay, constant);
 		} while (!constant.equals("exit"));
 		
@@ -53,6 +75,20 @@ public class Server {
 		System.out.println("Received " + constant + " from the client, processing...");
 	}
 	
+	public String getStringFromRemote() {
+		if (bufferRead == null)
+			return null;
+		try {
+			return (bufferRead.readLine());
+		} catch (IOException e) { e.printStackTrace(); }
+		return null;
+	}
 	
+	public Socket getSocket () {
+		return this.socketCliente;
+	}
 	
+	public void setBufferReader(BufferedReader br) {
+		bufferRead = br;
+	}
 }
