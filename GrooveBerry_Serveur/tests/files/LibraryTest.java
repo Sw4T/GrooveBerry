724870612f1736ed2342e5013d9f77a5315f127b
smn0078a <smn0078a@r-info-rubi-e19.iut.iut-tlse3.fr>
2015-01-29 12:41:48 +0100

    Add ArrayList<AudioFile> constructor to ReadingQueue

--- GrooveBerry_Serveur/tests/files/LibraryTest.java
+++ GrooveBerry_Serveur/tests/files/LibraryTest.java
@@ -2,23 +2,43 @@ package files;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class LibraryTest {
+	private Library library;
 
 	@Before
 	public void setUp() throws Exception {
+		this.library = new Library();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		this.library = null;
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDefaultConstructorCreateAnEmptyLibrary() {
+		this.library = new Library();
+		
+		assertTrue(this.library.isEmpty());
+	}
+	
+	@Test
+	public void testDefaultConstructorCreateEmptyFileInSystem() {
+		
+	}
+	
+	@Test
+	public void testCreateALibraryWithAudioFile() {
+		ArrayList<AudioFile> audioFileList = new ArrayList<AudioFile>();
+		this.library = new Library(audioFileList);
+		
+		assertEquals(audioFileList, this.library.getAudioFileList());
 	}
 
 }
