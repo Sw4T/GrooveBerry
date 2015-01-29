b0b884dccb07b07e20f9cef6b12d03bd10c79cbb
gmp0836a <gmp0836a@r-info-rubi-l18.iut.iut-tlse3.fr>
2015-01-29 16:06:30 +0100

    Ajout de la procédure de récupération de la liste de lecture et changement d'affichage + classe de réception/transmission

--- GrooveBerry_Serveur/src/network/ClientTest.java
+++ GrooveBerry_Serveur/src/network/ClientTest.java
@@ -1,8 +1,11 @@
 package network;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -10,7 +13,6 @@ public class ClientTest {
 
 	private Socket socket;
 	private PrintWriter printer;
-	public final static String fileToTest = "audio/Bob Marley - Jammin.mp3";
 	
 	public void connectionServer() {
 		try {
@@ -28,20 +30,31 @@ public class ClientTest {
 	}
 	
 	//Main du client Android ici simulé 
+	@SuppressWarnings("unchecked")
 	public static void main (String [] args) {
 		Socket socket = null;
 		Scanner scan = new Scanner(System.in);
 		int entreeUser = 0; String treatment = null;
-		
+		ArrayList<String> listReading;
 		try {
 			//Connexion au serveur
 			socket = new Socket("localhost", Server.SERVER_PORT);
 			if (socket.isConnected())
 				System.out.println("Client : Je me suis bien connecté au serveur ! youhou!");
 			
-			//Envoi de la chaine correspondant au chemin du fichier pouvant etre lu sur le serveur
-			PrintWriter printer = new PrintWriter(socket.getOutputStream(), true);
-			printer.println(fileToTest);
+			//Déclaration des buffers entrées/sorties et réception de la liste
+			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+			
+			//Reception du fil de lecture depuis le serveur
+			if (in.readUTF().equals("#RQ")) {
+				out.writeUTF("#OK");
+				out.flush();
+				listReading = (ArrayList<String>) in.readObject();
+				if (listReading != null)
+					showReadingQueue(listReading);
+			} else
+				System.out.println("Erreurs de synchronisation serveur !");
 			
 			//Envoi de chaines définissant des constantes au serveur
 			do {
@@ -52,17 +65,20 @@ public class ClientTest {
 				} catch (InputMismatchException inputFail) {
 					treatment = "";
 				}
-				if (!treatment.equals(""))
-					printer.println(treatment);
+				if (!treatment.equals("")) {
+					out.writeUTF(treatment);
+					out.flush();
+				}
 			} while (entreeUser != 7);
 			
 			//Fermeture de la connexion avec le serveur
 			if (socket != null) {
-				printer.close();
+				in.close();
+				out.close();
 				socket.close();
 				scan.close();
 			}
-		} catch (IOException e) {
+		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		} 
 	}
@@ -96,6 +112,14 @@ public class ClientTest {
 		System.out.println("7. Exit");
 		System.out.println("8. Next");
 		System.out.println("9. Prev");
+	}
+	
+	public static void showReadingQueue(ArrayList<String> list) 
+	{
+		System.out.println("Liste de lecture du serveur : ");
+		for (int i = 1; i <= list.size(); i++) {
+			System.out.println("\t" + i + ". " + list.get(i - 1));
+		}
 	}
 	
 	public Socket getSocket() {
