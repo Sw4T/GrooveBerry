8379a90a44f73ac8e6fdd0924fc3e7031cb30184
gmp0836a <gmp0836a@r-info-rubi-l18.iut.iut-tlse3.fr>
2015-02-24 17:19:07 +0100

    Update

--- GrooveBerry_Serveur/src/protocol/Notifier.java
+++ GrooveBerry_Serveur/src/protocol/Notifier.java
@@ -1,16 +1,11 @@
 package protocol;
 
-import java.util.ArrayList;
-
-import network.Client;
 
 public abstract class Notifier implements Runnable {
 	
-	protected ArrayList<Client> listClients;
 	protected Object [] toSend;
 	
-	public Notifier(ArrayList<Client> list, Object ... send) {
-		this.listClients = list;
+	public Notifier(Object [] send) {
 		this.toSend = send;
 	}
 	
--- GrooveBerry_Serveur/src/protocol/NotifierReadingQueue.java
+++ GrooveBerry_Serveur/src/protocol/NotifierReadingQueue.java
@@ -1,26 +1,23 @@
 package protocol;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import network.Client;
-import files.ReadingQueue;
+import network.Server;
 
 public class NotifierReadingQueue extends Notifier {
 
-	public NotifierReadingQueue(ArrayList<Client> list, Object ... send) {
-		super(list, send);
+	public NotifierReadingQueue(Object ... send) {
+		super(send);
 	}
 
 	public void run() {
 		synchronized (this) {
-			for (Client c : listClients) {
+			for (Client c : Server.listClients) {
 				if (c != null) {
 					try {
 						c.getOut().writeObject(Protocol.MODIFY_READING_QUEUE);
-						ReadingQueue readingQueue = (ReadingQueue) toSend[0];
-						c.getOut().writeObject(readingQueue); //Fil de lecture 
-						c.getOut().writeObject(readingQueue.getCurrentTrackPosition());
+						c.getOut().writeObject(Server.readingQueue); //Fil de lecture 
 						c.getOut().flush();
 						c.getOut().reset();
 					} catch (IOException e) {
