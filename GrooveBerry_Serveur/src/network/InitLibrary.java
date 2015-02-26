3f7e8a68e4f9623dd1a1b90d06ccf0233b2bea56
Seikomi <nicolas.symphorien@gmail.com>
2015-02-26 15:14:44 +0100

    Add InitLibrary class with a static class to initialize library.txt

---
+++ GrooveBerry_Serveur/src/network/InitLibrary.java
@@ -0,0 +1,32 @@
+package network;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import files.AudioFile;
+import files.Library;
+
+/*
+ * Use ones to initialize library file : res/library.txt
+ */
+
+public class InitLibrary {
+	public static void main(String[] args) {
+		ArrayList<AudioFile> audioFileList = new ArrayList<>();
+		audioFileList.add(new AudioFile("audio/04 Hey Joe.mp3"));
+		audioFileList.add(new AudioFile("audio/05 Mentira.mp3"));
+		audioFileList.add(new AudioFile("audio/Bob Marley - Jammin.mp3"));
+		audioFileList.add(new AudioFile("audio/free.wav"));
+		audioFileList.add(new AudioFile("audio/aol.wav"));
+		audioFileList.add(new AudioFile("audio/banane2.wav"));
+		
+		try {
+			Library lib = new Library(audioFileList);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
