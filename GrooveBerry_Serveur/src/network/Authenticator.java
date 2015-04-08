94be70c4f53fc743f230d5bda74d220b4a0b65c1
gmp0836a <gmp0836a@r-info-rubi-e19.iut.iut-tlse3.fr>
2015-04-08 18:21:24 +0200

    Authentification implémenté, délégation instantiation OK

--- GrooveBerry_Serveur/src/network/Authenticator.java
+++ GrooveBerry_Serveur/src/network/Authenticator.java
@@ -12,18 +12,16 @@ public class Authenticator implements Runnable {
 
 	private Socket socketSimple;
 	private Socket socketObject;
-	private Server server;
 	
-	public Authenticator(Socket socketSimple, Socket socketObject, Server server) {
+	public Authenticator(Socket socketSimple, Socket socketObject) {
 		this.socketSimple = socketSimple;
 		this.socketObject = socketObject;
-		this.server = server;
 	}
 	
 	@Override
 	public void run() {
 		try {
-			System.out.print("Authentification du client " + socketSimple.getInetAddress());
+			System.out.println("Authentification du client " + socketSimple.getInetAddress());
 			AuthenticationSystem authSystem = new AuthenticationSystem();
 			PrintWriter printer = new PrintWriter(socketSimple.getOutputStream(), true);
 			BufferedReader buffer = new BufferedReader(new InputStreamReader(socketSimple.getInputStream()));
@@ -33,22 +31,38 @@ public class Authenticator implements Runnable {
 			try {
 				String passwordReceived = buffer.readLine();
 				boolean passwordOK = authSystem.verifyPassword(passwordReceived);
+				System.out.println("Mot de passe reçu du client " + socketSimple.getInetAddress().getHostAddress() + " : " + passwordReceived);
+				Server server = Server.getInstance();
 				if (passwordOK) {
+					System.out.println("Authentification OK...");
 					Client newClient = new Client(socketSimple, socketObject, server);
 					newClient.setBuffers(printer, buffer);
-					new Thread(newClient).start();
-					System.out.println(" OK...");
+					
+					sendReadingQueueToRemote(newClient); 
+					server.updateClientList(newClient); 
+					new Thread(newClient).start(); //Lancement du traitement client
 				} else
-					System.out.println(" FAILED...");
+					System.out.println("Authentification FAILED...");
 			} catch (IOException e) {
 				e.printStackTrace();
 				System.out.println("Erreur lors de la réception du mot de passe venant du client !");
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.out.println();
+			System.out.println("Erreur lors de la création de flux avec le client !");
 		}
-		
+	}
+	
+	public void sendReadingQueueToRemote(Client c) 
+	{
+		c.sendSerializable("#RQ"); //Constante pour reading queue
+		String rep = (String) c.readSerializable();
+		if (rep.equals("#OK")) {
+			if (c.sendSerializable(Server.getInstance().getReadingQueue())) {
+				System.out.println("Envoi de la reading queue OK...");
+			}
+		} else
+			System.out.println("Erreur lors de l'envoi de la reading queue");
 	}
 
 }
